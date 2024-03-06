3b8236ddc5ba968fdd35e0403900850faecc66ae
khushitulshyan <ktulshyan123@gmail.com>
2024-03-06 18:04:46 +0530

    changed db host

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   password: "1234",
-  host: "127.0.0.1",
+  host: "host.docker.internal",
   port: 5432,
   database: "postgres"
 });
